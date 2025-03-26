531d3de82e6ff466beacfb7cad2880de223d80e4
Naruebet-s <107977472+bste101@users.noreply.github.com>
2025-03-26 15:31:10 +0700

    [Edit] Home Page

---
+++ src/app/(wow)/testnav/page.tsx
@@ -0,0 +1,12 @@
+import AppNavbar from "@/components/appnavbar/app-navbar";
+import React from "react";
+
+const ComponentName = () => {
+    return (
+        <>
+        <AppNavbar/>
+        </>
+    );
+};
+
+export default ComponentName;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,76 @@
+'use client';
+import AppNavbar from "@/components/appnavbar/app-navbar";
+import { useRouter } from "next/navigation";
 import { ThemeButton } from "@/components/button/theme-button";
 import Image from "next/image";
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import ProfilePage from "@/components/profilepage/profile-page";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="w-full flex flex-col justify-center items-center">
-      <div className="w-4/5">
+    <>
+      <AppNavbar />
+      <div className="w-full flex flex-col items-center">
         <ThemeButton />
-        <div className="flex justify-end items-center">
-          <Image
-            className="dark:invert"
-            src="/landing-page/robot.svg"
-            alt="robot"
-            width={105}
-            height={105}
-            priority
-          />
-          <div className="space-y-2">
-            <div className="text-[#201860] text-6xl font-bold">RAG Chat</div>
-            <div className="text-[#6B5F81] text-xl">
-              AI-powered chatbot for ENKKU
-            </div>
-          </div>
-        </div>
-
-        <div className="relative flex flex-col -top-6">
-          <div className="flex">
-            <Image
-              className="dark:invert"
-              src="/landing-page/kku-logo.svg"
-              alt="kku-logo"
-              width={105}
-              height={105}
-              priority
-            />
-            <Image
-              className="dark:invert"
-              src="/landing-page/enkku.svg"
-              alt="enkku"
-              width={105}
-              height={105}
-              priority
+        <div id="home" className="w-4/5 flex justify-between items-center mt-10">
+          <div className="w-1/3">
+            <DotLottieReact
+              src=".\animation\ai.lottie"
+              loop
+              autoplay
             />
           </div>
-          <div className="space-y-6">
-            <div className="text-4xl font-bold">
-              Welcome to the Future of AI at the Faculty of Engineering,
-              <br />
-              Khon Kaen University
-            </div>
-            <div className="text-[#6B5F81] text-xl">
-              Seamless conversations. Intelligent answers. A smarter way to
-              explore knowledge.
-            </div>
-          </div>
 
-          <div className="relative flex justify-start mt-6">
-            <button className="rounded-3xl border-4 border-[#9B27C6] px-12 py-1 font-bold text-xl">
+          <div className="w-2/3 space-y-4">
+            <div className="flex items-center space-x-2">
+              <span className="text-5xl font-bold text-[#8B1E3F]">RAG</span>
+              <span className="text-5xl font-bold text-[#1E3F8B]">Chat</span>
+              <span ><div className="flex items-center space-x-4 mt-6">
+                <span className="text-gray-500 text-sm">Powered by</span>
+                <Image
+                  src="/landing-page/kku-logo.svg"
+                  alt="kku-logo"
+                  width={50}
+                  height={50}
+                  priority
+                />
+                <Image
+                  src="/landing-page/enkku.svg"
+                  alt="enkku"
+                  width={50}
+                  height={50}
+                  priority
+                />
+              </div></span>
+            </div>
+            <div className="text-gray-700 font-semibold text-lg">
+              <span className="text-[#5A2D59] font-bold">
+                Welcome to the Future of AI at the Faculty of Engineering,
+                Khon Kaen University
+              </span>
+            </div>
+            <p className="text-gray-600 text-lg">
+              Seamless conversations. Intelligent answers. A smarter way to explore knowledge.
+            </p>
+
+            <button
+              className="rounded-full border-2 border-purple-500 text-purple-500 px-6 py-2 text-lg font-semibold hover:bg-purple-500 hover:text-white transition"
+              onClick={() => router.push('/testside')}
+            >
               Get Started
             </button>
-            <Image
-              src="/landing-page/cursor.svg" // ไฟล์ cursor
-              alt="cursor"
-              width={70}
-              height={70}
-              className="absolute left-44 top-6"
-            />
           </div>
         </div>
+        <div id="about" className="mt-36 mb-36 flex flex-col items-center text-center">
+          <div className="text-3xl font-bold text-purple-600 mb-6">About</div>
+          <ProfilePage />
+        </div>
+        <div id="help" className="mt-36 mb-36">
+          <div className="text-3xl font-bold text-purple-600">Help</div>
+        </div>
       </div>
-      <div className="w-full absolute bottom-0">
-        <Image
-          src="/landing-page/wave.svg"
-          alt="wave"
-          width={10}
-          height={10}
-          className="w-full absolute bottom-0 z-0"
-        />
-      </div>
-    </div>
+    </>
   );
 }
